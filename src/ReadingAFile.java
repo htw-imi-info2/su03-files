15e0e52b89bb86e0fdbc42e40d84a0353daac505
B Kleinen <kleinen@infrastructure.de>
2017-04-07 07:10:31 +0200

    initial commit

---
+++ src/ReadingAFile.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ReadingAFile {
+	
+	public static void main(String[] args) {
+		
+		Path path = Paths.get("data/colors.txt");
+		
+		showPathInfo(path);
+		
+		//String content = new String(Files.readAllBytes(path));
+		
+	}
+
+	private static void showPathInfo(Path path) {
+		System.out.format("toString: %s%n", path.toString());
+		System.out.format("getFileName: %s%n", path.getFileName());
+		System.out.format("getName(0): %s%n", path.getName(0));
+		System.out.format("getNameCount: %d%n", path.getNameCount());
+		System.out.format("subpath(0,2): %s%n", path.subpath(0,2));
+		System.out.format("getParent: %s%n", path.getParent());
+		System.out.format("getRoot: %s%n", path.getRoot());
+		//System.out.format("toRealPath: %s%n", path.toRealPath());	
+	}
+
+}
